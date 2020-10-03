d46d9fce2c243047c9e25eb0dfd5af913845aed0
jamie.sgro <iamjamies@gmail.com>
2020-10-03 13:49:53 -0400

    further built out Anagram() to include a msg about why the anagram passed or failed

--- src/models/Anagram.ts
+++ src/models/Anagram.ts
@@ -1,15 +1,31 @@
 import { IAnagram } from "./IAnagram";
 
 export class Anagram implements IAnagram {
-  word1: string;
-  word2: string;
-
   public constructor(word1, word2) {
     this.word1 = word1;
     this.word2 = word2;
   }
+  
+  word1: string;
+  word2: string;
+  private statusMsg: string = "";
+
+  public isValidWord(word: string): boolean {
+    // Must be a single word
+    // Must be a dictionary word (english)
+    // Must be type string (check for null too)
+    // Must NOT have number or symbols
+
+    throw new Error("Method not implemented.");
+  }
 
   public isAnagram(): boolean {
-    throw new Error("Method not implemented");
+    // Two of the same word are also not anagrams
+
+    throw new Error("Method not implemented.");
+  }
+
+  public getStatusMsg(): string {
+    throw new Error("Method not implemented.");
   }
 }
