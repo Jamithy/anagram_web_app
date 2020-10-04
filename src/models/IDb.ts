b1e22422f357ad88f2008876b58a2eb1cd8aace9
jamie.sgro <iamjamies@gmail.com>
2020-10-04 14:45:18 -0400

    Building out sql queries to a db rewritten each build

--- src/models/IDb.ts
+++ src/models/IDb.ts
@@ -1,16 +1,16 @@
 /** Interface for this servers persistent (or in memory) database  */
-export interface IDb {
+export interface IDb<T> {
   /** Create db from scratch */
-  init(): boolean;
+  init(): void;
 
   /** Add new record to a current db */
-  create(): void;
+  create(data: T): void;
 
   /** Retrieve currently existing records from db */
-  read<T>(): T;
+  read(): void;
 
   /** Mutate data within a currently existing record existing with db */
-  update(): void;
+  update(data: T): void;
 
   /** Delete records from current db */
   destroy(): void;
--- src/models/SqliteDb.ts
+++ src/models/SqliteDb.ts
@@ -1,23 +1,71 @@
 import { IDb } from "./IDb";
+import * as sqlite3 from 'sqlite3';
+import { IAnagramModel } from "./IAnagram";
 
-export class SqliteDb implements IDb {
-  init(): boolean {
+export class SqliteDb implements IDb<IAnagramModel> {
+  constructor() {
+    this._db = this.getDb();
+  }
+
+  private _db : Promise<sqlite3.Database>;
+
+  private async getDb(): Promise<sqlite3.Database> {
+    return new sqlite3.Database('./db/anagram.db', (err) => {
+      if (err) {
+        return console.error(err.message);
+      }
+    });
+  }
+  
+  public async init() {
+    let sql = `CREATE TABLE IF NOT EXISTS 'anagram_pair' (
+              'id'	    INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE,
+              'word_1'	TEXT NOT NULL,
+              'word_2'	TEXT NOT NULL,
+              'pairing' TEXT NOT NULL
+              );
+              PRAGMA user_version = 1;`;
+    (await this._db).all(sql, [], async (err, rows) => {
+      if (err) {
+        (await this._db).close();
+        throw err;
+      }
+      console.log('Creating table in in-memory SQlite database.');
+    });
+  }
+
+  public async create(data: IAnagramModel) {
+    // Sort the pairs so reverse pairings are considered the same
+    let sorted = [data.word1, data.word2].sort();
+    let sql = `INSERT INTO 'anagram_pair' (word_1, word_2, pairing)
+               VALUES ('${sorted[0]}', '${sorted[1]}', '${sorted[0]}|${sorted[1]}');`;
+    (await this._db).all(sql, [], async (err, rows) => {
+      if (err) {
+        (await this._db).close();
+        throw err;
+      }
+    });
+  }
+
+  public async read() {
+    //let words = Factory.createAnagramModel();
+    let sql = `SELECT word_1, word_2 FROM 'anagram_pair'`;
+    (await this._db).all(sql, [], async (err, rows) => {
+      if (err) {
+        (await this._db).close();
+        throw err;
+      }
+      rows.forEach((row) => {
+        console.log(row);
+      });
+    });
+  }
+
+  public async update() {
     throw new Error("Method not implemented.");
   }
 
-  create(): void {
-    throw new Error("Method not implemented.");
-  }
-
-  read<T>(): T {
-    throw new Error("Method not implemented.");
-  }
-
-  update(): void {
-    throw new Error("Method not implemented.");
-  }
-
-  destroy(): void {
+  public async destroy() {
     throw new Error("Method not implemented.");
   }
 }
